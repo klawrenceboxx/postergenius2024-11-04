7f31f0c94940ab0b1104f8d2cce3a8fea9329c08
Kaleel Lawrence-boxx <kaleellawrence-boxx@hotmail.ca>
2024-11-12 20:02:06 -0500

    adding a top component to hold the navigation bar

---
+++ app/components/header/Top.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Top: React.FC = () => {
+  return (
+    <div className="flex">
+      <div className="flex">
+        <div></div>
+        <ul className="flex">
+          <li>
+            <span>Canada/cad</span>
+          </li>
+          <li>
+            <span>Buyer Protection</span>
+          </li>
+          <li>
+            <span>Customer Service</span>
+          </li>
+          <li>
+            <span>Help</span>
+          </li>
+          <li>
+            <span>Wishlist</span>
+          </li>
+          <li>
+            <span>Account</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Top;
