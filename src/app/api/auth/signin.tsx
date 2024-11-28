bb9bccd40a1aa925fc075e6000198ddf5c799bc2
Kaleel Lawrence-boxx <kaleellawrence-boxx@hotmail.ca>
2024-11-27 19:39:54 -0500

    made adjustments before trying to follow along with the authentication. I messed something up. started on the home page content. created a home component started small

---
+++ src/app/api/auth/signin.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "../../../components/header/index";
+import Footer from "../../../components/footer/index";
+
+function signin() {
+  return (
+    <>
+      <Header country="canada" />
+      <div>
+        <div>
+          <div>
+            <p>Get access to one of the best Eshopping services in the world</p>
+            <Formik>
+              <Form>
+                <LoginInput></LoginInput>
+              </Form>
+            </Formik>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default signin;
---
+++ src/components/home/main/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function index() {
+  return (
+    <div>
+      <div>header</div>
+      <div>menu</div>
+      <div>swiper</div>
+      <div>offers</div>
+      <div>user</div>
+    </div>
+  );
+}
+
+export default index;
